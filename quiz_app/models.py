2e67dc9960330465d24617ad933d0ea28c37c06a
Mohammadamin <biglarymohamadamin75@gmail.com>
2021-11-24 10:15:07 +0330

    Model Quiz added to Models.py

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 
 class Quiz(models.Model):
-    Title=models.CharField(max_length=100)
-    Category=models.CharField(max_length=100)
-    
+    Question=models.CharField(max_length=200)
+    op1     =models.CharField(max_length=200)
+    op2     =models.CharField(max_length=200)
+    op3     =models.CharField(max_length=200)
+    op4     =models.CharField(max_length=200)
+    Category=models.CharField(max_length=200)
+    Answer  =models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.Question
